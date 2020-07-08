9a70b793be5e15cd412d06b8bc1d7c0e3634df82
KeithKoltweit <67757583+KeithKoltweit@users.noreply.github.com>
2020-07-08 12:01:59 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// create currency conversion
+function currencyConvert(dols) {
+    // gbp as of 2/28/2020 
+    let gbp = .78;
+    // do math and pass result to html
+    converted.textContent = dols * gbp;
+}
